6c325e9d18c943dfa273cd02552a58a0e3c1d808
Prasad Surase <prasad@joshsoftware.com>
2011-01-20 15:02:56 +0530

    modified repositories controller to disallow empty and invalidly structured git urls

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,7 @@
 class RepositoriesController < ApplicationController
   # GET /repositories
   # GET /repositories.xml
-  
+
   before_filter :init
   def init
     @selected = 'repository'
@@ -55,23 +55,36 @@ class RepositoriesController < ApplicationController
 
 
   # user defined method th parse, verify the git url and populate the repository
-  def create
-    if Repository.find_by_url(params['repository']['url']) == nil
+=begin
+def create
+    # if git url is not present in db, create new repo and assign current user as owner
+    if (@repo = Repository.find_by_url(params['repository']['url'])) == nil
       @repo = Repository.new(params['repository'])
-
-    else
+      @repo.user = current_user
+    else  #else if old git url, load it. 
       flash[:notice] = "Repository Present"
-      @repo = Repository.find_by_url(params['repository']['url'])
+      #    @repo = Repository.find_by_url(params['repository']['url'])
     end
 
-    repo_name = /^\w*[@|:][\/\/]*github.com[:?\/]*(([\w]+)\/([\w\-\.]+)).git$/.match(params['repository']['url']).captures
-    @repo.name = repo_name.first if @repo.name.blank?
+    #if url does not match given structure, flash notice and redirect to index
+    if /^\w*[@|:][\/\/]*github.com[:?\/]*(([\w]+)\/([\w\-\.]+)).git$/.match(params['repository']['url']) == nil
+      flash[:notice] = "Repository Link Invalid"
+      redirect_to :action => "index"
+    else # if matches the structure then extract the author name and project name in repo_name
+      repo_name = /^\w*[@|:][\/\/]*github.com[:?\/]*(([\w]+)\/([\w\-\.]+)).git$/.match(params['repository']['url']).match.captures
+    end
+
+    if repo_name.first.blank? # if repo_name.first is blank, flash error and redirect to index
+      flash[:notice] = "GitLog Url Cannot Be Blank"
+      redirect_to :action => "index"
+    end
+
+    @repo.name = repo_name.first
     str = "http://github.com/api/v2/json/commits/list/" + repo_name.first  + "/master"
     url = URI.parse(str)
     res = Net::HTTP.get_response(url)
-    if res.class == Net::HTTPOK
-      @repo.user = current_user
-      @repo.save
+    if res.class == Net::HTTPOK #the json url is valid if response is OK
+      @repo.save  
       @author_repository = AuthorRepository.new
       commits = MultiJson.decode(res.body)
       commits['commits'].each do |commit|
@@ -96,5 +109,54 @@ class RepositoriesController < ApplicationController
       flash[:notice] = "Repository Invalid"
     end
     redirect_to :action => "index"
-  end
-end
+=end
+
+  def create
+    #if url passed is not blank and matches the structure then   
+    if !params['repository']['url'].blank? and  /^\w*[@|:][\/\/]*github.com[:?\/]*(([\w]+)\/([\w\-\.]+)).git$/.match(params['repository']['url'])
+      repo_name = /^\w*[@|:][\/\/]*github.com[:?\/]*(([\w]+)\/([\w\-\.]+)).git$/.match(params['repository']['url']).captures.first
+      str = "http://github.com/api/v2/json/commits/list/" + repo_name + "/master"
+      url = URI.parse(str)
+      res = Net::HTTP.get_response(url)
+      #if url not found
+      if res.class == Net::HTTPNotFound
+        flash[:notice] = "Invalid Url"
+        redirect_to :action => "index"
+        return
+      elsif res.class == Net::HTTPOK #url found 
+        #if found in database 
+        if (@repo = Repository.find_by_url(params['repository']['url'])) != nil
+          flash[:notice] = "present in database"
+          redirect_to :action => "index"
+          return
+
+        else
+          @repo = Repository.new(params['repository'])
+          @repo.name = repo_name
+          @repo.user = current_user 
+          @repo.save
+          @author_repository = AuthorRepository.new
+          commits = MultiJson.decode(res.body)
+          commits['commits'].each do |commit|
+            @git_log = GitLog.new
+            @git_log.sha = commit['id']
+            @git_log.comment = commit['message']
+            @git_log.committed_at = commit['committed_date']
+            @author = Author.find_by_name(commit['author']['name']) 
+            if @author == nil 
+              @author = Author.new
+              @author.name = commit['author']['name']
+              @author.email = commit['author']['email']
+              @author.save
+            end
+            @git_log.author = @author_repository.author = @author
+            @git_log.repository = @author_repository.repository = @repo
+            @author_repository.save
+            @git_log.save
+          end#do
+        end#inner if
+      end#elsif
+    end#if
+    redirect_to :action => "index"
+  end#def
+end#class
